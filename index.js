2c2b9ed7ff239d098d306ef5be7ae8a0e30f5568
anilalochna <154051755+anilalochna@users.noreply.github.com>
2024-12-30 09:00:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const prefix = '.'
 const ownerNumber = ['94702940582']
 
 //===================SESSION-AUTH============================
-if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
+if (!fs.existsSync(__dirname + '/Session/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
-fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+fs.writeFile(__dirname + '/Session/creds.json', data, () => {
 console.log("Session downloaded ✅")
 })})}
 
